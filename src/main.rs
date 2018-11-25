776497639aa3ad7dd9213474dedbe80565c2e19d
Jeremy Cowgar <jeremy@cowgar.com>
2018-11-25 09:43:21 -0500

    Break code into modules

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,7 @@
 #[macro_use] extern crate lazy_static;
 extern crate regex;
 
-use std::collections::HashMap;
-use regex::Regex;
-
-#[derive(Debug)]
-struct Todo {
-    is_complete: bool,
-    task       : String,
-    priority   : Option<char>,
-    projects   : Vec<String>,
-    contexts   : Vec<String>,
-    key_values : HashMap<String, String>,
-}
-
-lazy_static! {
-    static ref PARSE_RE:      Regex = Regex::new(r"^(?P<complete>x )?(?:\((?P<priority>[A-Z])\))?\s*(?P<date1>\d{4}-\d{2}-\d{2})?\s*(?P<date2>\d{4}-\d{2}-\d{2})?\s*(?P<task>.+$)").unwrap();
-    static ref PROJECTS_RE:   Regex = Regex::new(r"+(\w+)").unwrap();
-    static ref CONTEXTS_RE:   Regex = Regex::new(r"@(\w+)").unwrap();
-    static ref KEY_VALUES_RE: Regex = Regex::new(r"(?P<key>\w+):(?P<value>\w+)").unwrap();
-}
-
-fn parse(line: &str) -> Option<Todo> {
-  let m = PARSE_RE.captures(line)?;
-    
-	let task = match m.name("task") {
-		None    => return None,
-		Some(t) => String::from(t.as_str()),
-	};
-
-  let projects    = Vec::new();
-  let contexts    = Vec::new();
-  let key_values  = HashMap::new();
-  let is_complete = m.name("complete").is_some();
-  let priority    = m.name("priority").map(|p| p.as_str().chars().next().unwrap());
-
-	Some(Todo {
-	    is_complete,
-	    task,
-	    priority,
-	    projects,
-	    contexts,
-	    key_values,
-	})
-}
+mod todo;
 
 fn main() {
     let examples = [
@@ -51,12 +9,15 @@ fn main() {
         "x (B) Get tires on the van @maintenance",
         "Learn Rust",
     ];
+    
+    let parsed = examples.iter()
+    	.map(|v| todo::Todo::parse(v))
+    	.filter(|v| v.is_some())
+    	.collect::<Vec<Option<todo::Todo>>>();
 
-    for example in examples.iter() {
-        let t = parse(example);
-
-        match t {
-	        None => println!("Could not parse: {}", example),
+    for example in parsed.iter() {
+        match example {
+	        None => println!("Could not parse"),
 	        Some(task) => println!("{:?}", task),
         };
     }
---
+++ src/todo.rs
@@ -0,0 +1,60 @@
+use std::collections::HashMap;
+use regex::Regex;
+
+lazy_static! {
+    static ref PARSE_RE:      Regex = Regex::new(r"^(?P<complete>x )?(?:\((?P<priority>[A-Z])\))?\s*(?P<date1>\d{4}-\d{2}-\d{2})?\s*(?P<date2>\d{4}-\d{2}-\d{2})?\s*(?P<task>.+$)").unwrap();
+    static ref PROJECTS_RE:   Regex = Regex::new(r"+(\w+)").unwrap();
+    static ref CONTEXTS_RE:   Regex = Regex::new(r"@(\w+)").unwrap();
+    static ref KEY_VALUES_RE: Regex = Regex::new(r"(?P<key>\w+):(?P<value>\w+)").unwrap();
+}
+
+#[derive(Debug)]
+pub struct Todo {
+	/// true if todo item is done.
+  is_complete: bool,
+
+  /// Task title
+  task       : String,
+  
+  /// Priority (if any), A-Z.
+  priority   : Option<char>,
+
+  /// Project tags (+Project)
+  projects   : Vec<String>,
+
+  /// Context tags (@context)
+  contexts   : Vec<String>,
+
+  /// Key value attributes (key:value)
+  key_values : HashMap<String, String>,
+}
+
+impl Todo {
+  /// Create a new Todo structure from the given raw line.
+  ///
+  /// The method may return None, if the line could not be parsed.
+  pub fn parse(line: &str) -> Option<Todo> {
+    let m = PARSE_RE.captures(line)?;
+      
+  	let task = match m.name("task") {
+  		None    => return None,
+  		Some(t) => String::from(t.as_str()),
+  	};
+
+    let projects    = Vec::new();
+    let contexts    = Vec::new();
+    let key_values  = HashMap::new();
+    let is_complete = m.name("complete").is_some();
+    let priority    = m.name("priority").map(|p| p.as_str().chars().next().unwrap());
+
+  	Some(Todo {
+  	    is_complete,
+  	    task,
+  	    priority,
+  	    projects,
+  	    contexts,
+  	    key_values,
+  	})
+  }
+}
+
