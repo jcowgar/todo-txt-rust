55219c23a3966ffe037253ae3e045ad144cd4511
Jeremy Cowgar <jeremy@cowgar.com>
2018-11-27 14:07:02 -0500

    Add working version of rm
    Not pretty, will revisit later

--- src/cmd_rm.rs
+++ src/cmd_rm.rs
@@ -1,11 +1,18 @@
 use gumdrop::Options;
-use todo_file::TodoFile;
+use todo_file::{parse_todos_from_default_file, write_todos_to_default_file};
 
 #[derive(Debug, Options)]
 pub struct Opts {
     #[options(help = "Id of todo to remove")]
-    id: i32,
+    id: usize,
 }
 
 pub fn execute(opts: &Opts) {
+  let todos = &mut parse_todos_from_default_file()
+    .expect("Could not parse todos from default file");
+
+  todos.remove(opts.id - 1);
+
+  write_todos_to_default_file(&todos)
+    .expect("Could not write todos to default file");
 }
