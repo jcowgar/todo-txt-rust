4d0459a8149eb24662ba0d9baeaccfbb57f5550d
Jeremy Cowgar <jeremy@cowgar.com>
2018-11-27 22:45:39 -0500

    Parse ids from extras in do command instead of --id=

--- src/cmd_do.rs
+++ src/cmd_do.rs
@@ -3,16 +3,19 @@ use todo_file::{parse_todos_from_default_file, write_todos_to_default_file};
 
 #[derive(Debug, Options)]
 pub struct Opts {
-    #[options(help = "Id of todo to mark complete")]
-    id: usize,
+    #[options(free)]
+    free: Vec<String>,
 }
 
 pub fn execute(opts: &Opts) {
   let todos = &mut parse_todos_from_default_file()
     .expect("Could not parse todos from default file");
-
-  if let Some(elem) = todos.get_mut(opts.id - 1) {
-    elem.is_complete = !elem.is_complete;
+  
+  for id in &opts.free {
+    let iid = id.parse::<usize>().unwrap();
+    if let Some(t) = todos.get_mut(iid - 1) {
+      t.is_complete = !t.is_complete;
+    }
   }
 
   write_todos_to_default_file(&todos)
