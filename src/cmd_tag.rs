d121cddadd8b55021a44437a9aa762592f442987
Jeremy Cowgar <jeremy@cowgar.com>
2018-11-29 01:45:45 -0500

    Parse tag name from first free option in tag command instead of from --tag

--- src/cmd_tag.rs
+++ src/cmd_tag.rs
@@ -6,14 +6,15 @@ use todo_file::{parse_todos_from_default_file, write_todos_to_default_file};
 pub struct Opts {
 	#[options(free)]
 	free: Vec<String>,
-
-	#[options(help = "Tag name")]
-	tag: String,
 }
 
 pub fn execute(opts: &Opts) {
-	let tag_name = &opts.tag;
-	let tag_name_formatted = format!(" #{}", opts.tag);
+	let mut free_it = opts.free.iter();
+	let tag_name = match free_it.next() {
+		Some(v) => v,
+		None => panic!("No tag name given!")
+	};
+	let tag_name_formatted = format!(" #{}", tag_name);
 	let mutually_exclusive_tags: Vec<Vec<String>> = get_mutually_exclusive_tags()
 		.into_iter()
 		.filter(|t| t.contains(&tag_name))
@@ -21,7 +22,7 @@ pub fn execute(opts: &Opts) {
 	let todos =
 		&mut parse_todos_from_default_file().expect("Could not parse todos from default file");
 
-	for id in &opts.free {
+	for id in free_it {
 		let iid = id.parse::<usize>().unwrap();
 		if let Some(t) = todos.get_mut(iid - 1) {
 			for tags in &mutually_exclusive_tags {
